747e494f455af9ab06b5f03856ef557ac49c88eb
Mohamad Fazel <mdhesari99@gmail.com>
2020-01-08 13:13:06 +0330

    update styles and fix some bugs

--- src/components/MasnoryItem.js
+++ src/components/MasnoryItem.js
@@ -1,15 +1,13 @@
 import React from "react"
-import { Image } from "gatsby-image"
 
 export default ({ work }) => {
   const { pictures, title, description, slug } = work
 
-  console.log(pictures)
   return (
     <div className="masonry-items pb-5">
       <div className="item">
         <img src={pictures[0].fluid.src} />
-        <a href={"/".slug} class="item-wrapper">
+        <a href={"/works/".slug} class="item-wrapper">
           <div class="item-inner">
             <h5>{title}</h5>
             <p>{description}</p>
--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 export default () => (
-  <section className="contact py-5 mt-5">
+  <section className="contact py-5">
     <div className="container">
       <div className="inner-contact text-center py-5">
         <h3>Can I help you there?</h3>
