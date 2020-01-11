04e8c90079a1cdaf25a94f6086ac24d660bcb0bd
Mohamad Fazel <mdhesari99@gmail.com>
2020-01-11 13:05:29 +0330

    sperate all scss and sinlge article ui enhancement

--- src/templates/article-template.js
+++ src/templates/article-template.js
@@ -2,37 +2,63 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
+import Img from "gatsby-image"
+import moment from "moment"
+import { FaCalendar, FaBookReader } from "react-icons/fa"
+import readTime from "reading-time"
 
 export default ({ data }) => {
-  let { articles } = data
-
+  let { article } = data
+  let reading_time = readTime(article.body.body)
   return (
     <Layout>
-      <SEO title={articles.title} />
-      <h1>{articles.title}</h1>
-      <p
-        dangerouslySetInnerHTML={{
-          __html: articles.body.childMarkdownRemark.html,
-        }}
-      ></p>
+      <SEO title={article.title} />
+      <div className="container">
+        <article className="post post-full">
+          <header className="post-header">
+            <h1 className="post-title">{article.title}</h1>
+            <div className="post-meta">
+              <time
+                className="published"
+                dateTime={moment(article.createdAt).format("LL")}
+              >
+                <FaCalendar className="post-meta-icon" />
+                {moment(article.createdAt).format("LL")}
+              </time>
+              <span>
+                <FaBookReader className="post-meta-icon" />
+                {reading_time.text}
+              </span>
+            </div>
+          </header>
+          <Img fluid={article.thumbnailImage.fluid} />
+          <div
+            className="post-content"
+            dangerouslySetInnerHTML={{
+              __html: article.body.childMarkdownRemark.html,
+            }}
+          ></div>
+        </article>
+      </div>
     </Layout>
   )
 }
 
 export const query = graphql`
   query($slug: String!) {
-    articles: contentfulArticles(slug: { eq: $slug }) {
+    article: contentfulArticles(slug: { eq: $slug }) {
       id: contentful_id
       excerpt
       title
       slug
       body {
+        body
         childMarkdownRemark {
           html
         }
       }
       thumbnailImage {
-        fluid {
+        fluid(quality: 100) {
           ...GatsbyContentfulFluid
         }
       }
