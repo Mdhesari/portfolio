79bb4dc557d967a607c4f8389e5380f19e9c6f3e
Mohamad Fazel <mdhesari99@gmail.com>
2020-01-13 12:41:56 +0330

    customize bootstrap scss and add buttons to work template

--- src/templates/work-template.js
+++ src/templates/work-template.js
@@ -3,11 +3,11 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 import Gallery from "../components/Gallery"
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa"
+import { FaArrowLeft, FaArrowRight, FaEye, FaGithub } from "react-icons/fa"
 import Container from "react-bootstrap/Container"
+import Linker from "../components/Linker"
 
 export default ({ data }) => {
-
   let { works } = data
 
   return (
@@ -37,22 +37,40 @@ export default ({ data }) => {
                   __html: works.text.childMarkdownRemark.html,
                 }}
               ></p>
-              {works.technologies !== null ? (
-                <>
-                  <h5 className="mt-4">Technologies : </h5>
-                  <ul className="list-group list-group-horizontal list-technologies list-technologies-single text-dark flex-flow-wrap">
-                    {works.technologies.map((item, index) => {
-                      return (
-                        <li key={index} className="list-group-item">
-                          {item}
-                        </li>
-                      )
-                    })}
-                  </ul>
-                </>
-              ) : (
-                ""
-              )}
+              <div className="post-meta">
+                {works.technologies !== null ? (
+                  <>
+                    <h5 className="mt-4">Technologies : </h5>
+                    <ul className="list-group list-group-horizontal list-technologies list-technologies-single text-dark flex-flow-wrap">
+                      {works.technologies.map((item, index) => {
+                        return (
+                          <li key={index} className="list-group-item">
+                            {item}
+                          </li>
+                        )
+                      })}
+                    </ul>
+                  </>
+                ) : (
+                  ""
+                )}
+                <div className="links mt-4">
+                  <Linker
+                    isLocal={false}
+                    url="#"
+                    className="btn btn-primary btn-sm py-1 px-3 btn-rounded"
+                  >
+                    Visit <FaEye />
+                  </Linker>
+                  <Linker
+                  isLocal={false}
+                  url="#"
+                  className="btn btn-secondary btn-sm py-1 px-3 btn-rounded float-right"
+                  >
+                    Source <FaGithub />
+                  </Linker>
+                </div>
+              </div>
             </div>
           </div>
         </article>
