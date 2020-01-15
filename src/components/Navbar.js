bcc7d89088f6609d39641aa4398c83082bee34c9
Mohamad Fazel <mdhesari99@gmail.com>
2020-01-15 17:10:34 +0330

    menu toggler add on bottom left

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,88 @@
 import React from "react"
 import { IoIosCall, IoIosMail } from "react-icons/io"
+import { Link } from "gatsby"
+import { FaToggleOn, FaBlog, FaRProject, FaHome, FaProjectDiagram, FaPaintBrush, FaMoon } from "react-icons/fa"
 
-export default () => (
-  <nav className="navbar navbar-expand navbar-light bg-light p-0">
-    <ul className="navbar-nav ">
-      <li className="nav-item">
-        <a
-          href="tel:+989105667705"
-          className="nav-link"
-          target="_blank"
-          rel="noopener noreferrer"
+export default class Navbar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.toggleMenu = this.toggleMenu.bind(this)
+    this.state = {
+      show: false,
+    }
+  }
+
+  toggleMenu() {
+    this.setState({ show: !this.state.show })
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-expand navbar-light bg-light p-0">
+        <button
+          onClick={this.toggleMenu}
+          id="main-navbar-toggler"
+          className={`navbar-toggler ${this.state.show ? "open-menu" : ""}`}
+          type="button"
         >
-          <IoIosCall />
-          &nbsp; +989105667705
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="mailto:contact@mdhesari.com"
-          className="nav-link"
-          target="_blank"
-          rel="noopener noreferrer"
+          <span className="icon-menu"></span>
+        </button>
+
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a
+              href="tel:+989105667705"
+              className="nav-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IoIosCall />
+              &nbsp; +989105667705
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="mailto:contact@mdhesari.com"
+              className="nav-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IoIosMail />
+              &nbsp; contact@mdhesari.com
+            </a>
+          </li>
+        </ul>
+
+        <div
+          className={`collapse navbar-collapse ${
+            this.state.show ? "show" : ""
+          }`}
+          id="contentNavbar"
         >
-          <IoIosMail />
-          &nbsp; contact@mdhesari.com
-        </a>
-      </li>
-    </ul>
-  </nav>
-)
+          <ul className="list-group">
+            <li className="list-group-item">
+              <Link to="/">
+                <FaHome />
+              </Link>
+            </li>
+            <li className="list-group-item">
+              <Link to="/articles">
+                <FaBlog />
+              </Link>
+            </li>
+            <li className="list-group-item">
+              <Link to="/works">
+                <FaPaintBrush />
+              </Link>
+            </li>
+            <li className="list-group-item">
+              <button type="button" className="btn btn-default">
+                <span><FaMoon /></span>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
